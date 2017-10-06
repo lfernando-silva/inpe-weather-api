09071e1147e9ef1b99194f3d18089911f349b076
Luiz Fernando <luiz.silva@smarti9.com.br>
2017-10-06 18:05:41 -0300

    cities list

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-module.exports = require('./lib/index.js')
+//module.exports = require('./lib/index.js')
+const InpeForecast = require('./lib/index.js')
+
+InpeForecast.listCities().then(console.log)
