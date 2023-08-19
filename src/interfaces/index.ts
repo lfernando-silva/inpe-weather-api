593beba2e77abc747a8cf5eccc875d4edf1cc255
Luiz Fernando <lfernando-silva@hotmail.com>
2023-08-19 13:49:28 -0300

    export interfaces as index

---
+++ src/interfaces/index.ts
@@ -0,0 +1,11 @@
+import { default as ICity } from './ICity';
+import { default as ICityForecast } from './ICityForecast';
+import { default as IForecastStatus } from './IForecastStatus';
+import { default as IWavesForecast } from './IWavesForecast';
+
+export { 
+    ICity,
+    ICityForecast,
+    IForecastStatus,
+    IWavesForecast
+}
