2c8a09d161082c397f9d9508ad72fbe6b6cdf45f
Luiz Fernando <lfernando-silva@hotmail.com>
2023-08-19 13:07:40 -0300

    make module compatible with js

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,5 +4,5 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   root: true,
-  ignorePatterns: ["lib/", 'jest.config.js']
+  ignorePatterns: ["dist/", 'jest.config.js']
 };
--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,10 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
+// eslint-disable-next-line no-undef
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testPathIgnorePatterns: [
+    "node_modules",
+    "dist"
+  ],
 };
